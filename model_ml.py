6a3599be6ab9b1d8ce5503c06c0cc3e002cf6298
Anuradha-Bharti1 <anuradhabharti2017@gmail.com>
2025-04-14 20:46:10 +0530

    added 3 new ml model

--- model_ml.py
+++ model_ml.py
@@ -1,1 +1,13 @@
 print("adding all model related code here")
+
+def model1():
+    print ("adding code for ML_model1")
+
+
+def model2():
+    print ("adding code for ML_model2")
+
+
+
+def model3():
+    print ("adding code for ML_model3")
